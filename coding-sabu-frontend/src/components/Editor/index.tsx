7d39932e4942c86cbc55f4bab233d53c329792b8
eotkd4791 <eotkd4791@gmail.com>
2021-11-22 21:17:46 +0900

    CS-2: Implementation toast ui editor

--- coding-sabu-frontend/src/components/Editor/index.tsx
+++ coding-sabu-frontend/src/components/Editor/index.tsx
@@ -1,12 +1,12 @@
 import { FC, useRef } from 'react';
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import Prism from 'prismjs';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-import 'prismjs/themes/prism.css';
 
 const Editor: FC = () => {
   const toastEditorRef = useRef<ToastEditor>(null);
--- coding-sabu-frontend/src/components/Viewer/index.tsx
+++ coding-sabu-frontend/src/components/Viewer/index.tsx
@@ -1,5 +1,6 @@
+import { FC } from 'react';
 import { Viewer as ToastViewer } from '@toast-ui/react-editor';
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import Prism from 'prismjs';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
@@ -7,8 +8,14 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import 'prismjs/themes/prism.css';
 
-const Viewer = () => {
-  return <ToastViewer initialValue='asdsad' plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]} />;
+interface Props {
+  initialValue: string;
+}
+
+const Viewer: FC<Props> = ({ initialValue }) => {
+  return (
+    <ToastViewer initialValue={initialValue} plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]} />
+  );
 };
 
 export default Viewer;
