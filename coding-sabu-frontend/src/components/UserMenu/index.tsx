5e66e843034ea1d9e42d5037d84c9a00b1c87e76
eotkd4791 <eotkd4791@gmail.com>
2021-11-23 19:21:03 +0900

    Add UserMenu component

---
+++ coding-sabu-frontend/src/components/UserMenu/index.tsx
@@ -0,0 +1,57 @@
+import { FC, useCallback } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { colors } from "../../styles/theme";
+
+const FlexContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UserMenuContainer = styled.ul`
+  width: 163px;
+  box-shadow: rgb(0 0 0 / 25%) 0px 0px 15px;
+  position: absolute;
+  top: 85px;
+  right: 10px;
+`;
+
+const UserMenuList = styled.li`
+  ${FlexContainer}
+  width: 100%;
+  height: 36px;
+  &:hover {
+    background-color: ${colors.yellow[4]};
+  }
+  cursor: pointer;
+`;
+
+const UserMenu: FC<RouteComponentProps> = ({ history }) => {
+  const navigatePage = useCallback(
+    path => {
+      history.push(path);
+    },
+    [history]
+  );
+
+  const logout = useCallback(() => {
+    // TODO: store의 유저 정보 삭제 로직 추가
+    history.replace("/");
+  }, [history]);
+
+  return (
+    <UserMenuContainer role='userMenu'>
+      <UserMenuList onClick={() => navigatePage("/mypage")}>
+        내 정보
+      </UserMenuList>
+      <UserMenuList onClick={() => navigatePage("/myclass")}>
+        내 가르침
+      </UserMenuList>
+      <UserMenuList onClick={logout}>로그아웃</UserMenuList>
+    </UserMenuContainer>
+  );
+};
+
+export default withRouter(UserMenu);
