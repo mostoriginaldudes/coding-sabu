b629a4bb2c5a9c2efa5bad9d8549a23a25082898
eotkd4791 <eotkd4791@gmail.com>
2021-12-01 23:13:41 +0900

    Add page for routing

--- coding-sabu-frontend/src/App.tsx
+++ coding-sabu-frontend/src/App.tsx
@@ -1,15 +1,29 @@
 import { FC } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import GlobalNav from './components/GlobalNav';
-import UserAuthForm from './components/UserAuthForm';
+import styled from '@emotion/styled';
+import { sizes } from './styles/theme';
+import Home from 'pages/Home';
+
+const Main = styled.main`
+  width: calc(100% - 2rem);
+  max-width: ${sizes.desktop}px;
+  margin: ${sizes.unitBig / 8}em auto 0;
+`;
 
 const App: FC = () => {
   return (
     <Router>
       <GlobalStyle />
       <GlobalNav />
-      <UserAuthForm />
+      <Main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Main>
     </Router>
   );
 };
