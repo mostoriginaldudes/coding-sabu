6c764f2331f2edb353549c4e4239139f5ee0e189
eotkd4791 <eotkd4791@gmail.com>
2022-02-04 03:49:04 +0900

    Remove typing about thunk

--- coding-sabu-frontend/src/store/index.ts
+++ coding-sabu-frontend/src/store/index.ts
@@ -10,14 +10,6 @@ export type ThunkAsyncState<T> = {
   error: Error | null;
 };
 
-export type ThunkAction = (dispatch: ThunkDispatch) => void;
-
-export interface ThunkDispatch {
-  (thunkAction: ThunkAction): void;
-  <T>(action: T): T;
-  <U>(action: U | ThunkAction): U;
-}
-
 const rootReducer = combineReducers({
   lesson: lessonReducer
 });
