33fc4b092208516cc5023869287b4f2fe05a98a1
eotkd4791 <eotkd4791@gmail.com>
2022-02-08 03:20:22 +0900

    Add file upload configuration

--- coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/CodingSabuBackendApplication.java
+++ coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/CodingSabuBackendApplication.java
@@ -1,8 +1,11 @@
 package com.mostoriginaldudes.codingsabubackend;
 
+import com.mostoriginaldudes.codingsabubackend.config.FileUploadConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({ FileUploadConfig.class })
 @SpringBootApplication
 public class CodingSabuBackendApplication {
 
