beafd8257aada9d4d37cd70a2642076e54053d06
eotkd4791 <eotkd4791@gmail.com>
2022-02-08 15:36:06 +0900

    Remove setter

--- coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/dto/request/EditUserInfoRequestDto.java
+++ coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/dto/request/EditUserInfoRequestDto.java
@@ -1,9 +1,7 @@
 package com.mostoriginaldudes.codingsabubackend.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
 public class EditUserInfoRequestDto {
   private int id;
