95bcd2643cc128888eced1d808df560335210156
eotkd4791 <eotkd4791@gmail.com>
2022-02-08 15:52:44 +0900

    Refactor readability

--- coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/controller/LessonController.java
+++ coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/controller/LessonController.java
@@ -42,7 +42,9 @@ public class LessonController {
     LessonResponseDto responseDto = lessonService.getLesson(lessonId);
 
     if(responseDto == null) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+      return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(null);
     }
     return ResponseEntity.ok(responseDto);
   }
