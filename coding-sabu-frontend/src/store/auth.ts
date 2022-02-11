272b68b03fe6597d8f97605a7807badf41d46418
eotkd4791 <eotkd4791@gmail.com>
2022-02-11 13:37:00 +0900

    Add store about auth

--- coding-sabu-frontend/src/store/auth.ts
+++ coding-sabu-frontend/src/store/auth.ts
@@ -1,40 +1,67 @@
-import { ThunkAsyncState, ThunkDispatch } from '.';
+import { ThunkAsyncState } from '.';
+import { ThunkAction } from 'redux-thunk';
+import { LoginInfo, SignupInfo, User } from 'types';
+import { login, signup } from 'apis';
 
 // constants
 const LOGIN = 'auth/LOGIN' as const;
 const LOGIN_FAIL = 'auth/LOGIN_FAIL' as const;
 const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS' as const;
+const SIGNUP = 'auth/SIGNUP' as const;
+const SIGNUP_FAIL = 'auth/SIGNUP_FAIL' as const;
+const SIGNUP_SUCCESS = 'auth/SIGNUP_SUCCESS' as const;
+const SET_TOKEN = 'auth/SET_TOKEN' as const;
 const LOGOUT = 'auth/LOGOUT' as const;
-const LOGOUT_SUCCESS = 'auth/LOGOUT_SUCCESS' as const;
-const LOGOUT_FAIL = 'auth/LOGOUT_FAIL' as const;
 
 // types
 interface State {
-  readonly authToken: ThunkAsyncState<string>;
-  readonly refreshToken: ThunkAsyncState<string>;
+  readonly token: string | null;
+  readonly user: ThunkAsyncState<User>;
 }
 
-type ActionType =
-  | typeof LOGIN
-  | typeof LOGIN_SUCCESS
-  | typeof LOGIN_FAIL
-  | typeof LOGOUT
-  | typeof LOGOUT_SUCCESS
-  | typeof LOGOUT_FAIL;
+type Action =
+  | { type: typeof LOGIN }
+  | { type: typeof LOGIN_SUCCESS; payload: User }
+  | { type: typeof LOGIN_FAIL; payload: Error }
+  | { type: typeof SIGNUP }
+  | { type: typeof SIGNUP_SUCCESS; payload: User }
+  | { type: typeof SIGNUP_FAIL; payload: Error }
+  | { type: typeof SET_TOKEN; payload: Pick<State, 'token'> }
+  | { type: typeof LOGOUT };
 
-interface Action {
-  type: ActionType;
-  payload?: State;
-}
+type AuthThunkAction = ThunkAction<void, State, null, Action>;
+
+// action creator
+export const createActionLogin =
+  (loginInfo: LoginInfo): AuthThunkAction =>
+  async dispatch => {
+    dispatch({ type: LOGIN });
+    try {
+      const user = await login(loginInfo);
+      dispatch({ type: LOGIN_SUCCESS, payload: user });
+    } catch (error) {
+      dispatch({ type: LOGIN_FAIL, payload: error as Error });
+    }
+  };
+
+export const createActionSignup =
+  (signupInfo: SignupInfo): AuthThunkAction =>
+  async dispatch => {
+    dispatch({ type: SIGNUP });
+    try {
+      const user = await signup(signupInfo);
+      dispatch({ type: SIGNUP_SUCCESS, payload: user });
+    } catch (error) {
+      dispatch({ type: SIGNUP_FAIL, payload: error as Error });
+    }
+  };
+
+export const createActionLogout = () => ({ type: LOGOUT });
 
 // state
 const initialState: State = {
-  authToken: {
-    loading: false,
-    data: null,
-    error: null
-  },
-  refreshToken: {
+  token: null,
+  user: {
     loading: false,
     data: null,
     error: null
@@ -50,37 +77,73 @@ export default function authReducer(
     case LOGIN:
       return {
         ...state,
-        refreshToken: action.payload
+        user: {
+          loading: true,
+          data: null,
+          error: null
+        }
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: {
+          loading: false,
+          data: action.payload,
+          error: null
+        }
+      };
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        user: {
+          loading: false,
+          data: null,
+          error: action.payload
+        }
+      };
+    case SIGNUP:
+      return {
+        ...state,
+        user: {
+          loading: true,
+          data: null,
+          error: null
+        }
+      };
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        user: {
+          loading: false,
+          data: action.payload,
+          error: null
+        }
+      };
+    case SIGNUP_FAIL:
+      return {
+        ...state,
+        user: {
+          loading: false,
+          data: null,
+          error: action.payload
+        }
+      };
+    case SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload
       };
     case LOGOUT:
       return {
         ...state,
-        authToken: null,
-        refreshToken: null
+        token: null,
+        user: {
+          loading: false,
+          data: null,
+          error: null
+        }
       };
     default:
       return state;
   }
 }
-
-// action creator
-export const login =
-  ({ email, password }: { email: string; password: string }) =>
-  (dispatch: ThunkDispatch) => {
-    dispatch({ loading: true, data: null, error: null });
-    try {
-      // TODO login request api
-      dispatch({ loading: false });
-    } catch (error) {}
-  };
-
-export const logout = () => (dispatch: ThunkDispatch) => {
-  dispatch({ type: LOGOUT });
-  try {
-    // TODO logout request api
-    dispatch({ type: LOGOUT_SUCCESS, data: null, error: null });
-    // TODO redirect to home
-  } catch (error) {
-    dispatch({ type: LOGOUT_FAIL, data: null, error: error });
-  }
-};
