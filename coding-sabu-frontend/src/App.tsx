e45fb2fcb7d3d66d8a35d357ff2c71291aedbc67
eotkd4791 <eotkd4791@gmail.com>
2022-02-14 06:43:25 +0900

    Add pages for routing, Refactor module path

--- coding-sabu-frontend/src/App.tsx
+++ coding-sabu-frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from 'store';
 import styled from '@emotion/styled';
-
-import GlobalNav from './components/GlobalNav';
+import GlobalNav from 'components/GlobalNav';
 import Home from 'pages/Home';
-import GlobalStyle from './styles/GlobalStyle';
-import { sizes } from './styles/theme';
-import HeadUpDisplay from 'components/HeadUpDisplay';
-import MyTeachingLessons from 'pages/MyTeachingLessons';
+import MyPage from 'pages/MyPage';
 import MyJoiningLessons from 'pages/MyJoiningLessons';
+import MyTeachingLessons from 'pages/MyTeachingLessons';
 import LessonForm from 'pages/LessonForm';
 import LessonDetail from 'pages/LessonDetail';
+import Logout from 'pages/Logout';
+import NotFound from 'pages/NotFound';
+import HeadUpDisplay from 'components/HeadUpDisplay';
+import store from 'store';
+import GlobalStyle from 'styles/GlobalStyle';
+import { sizes } from 'styles/theme';
 
 const Main = styled.main`
   width: calc(100% - 2rem);
@@ -30,10 +32,13 @@ const App: FC = () => {
           <HeadUpDisplay />
           <Switch>
             <Route path="/" component={Home} exact />
+            <Route path="/mypage" component={MyPage} exact />
             <Route path="/mylesson" component={MyJoiningLessons} />
             <Route path="/myteaching" component={MyTeachingLessons} />
             <Route path="/lesson/form" component={LessonForm} />
             <Route path="/lesson/:id" component={LessonDetail} />
+            <Route path="/logout" component={Logout} />
+            <Route path="*" component={NotFound} />
           </Switch>
         </Main>
       </Router>
