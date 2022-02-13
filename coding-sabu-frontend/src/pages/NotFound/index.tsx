baafc592a6f377e687cd6a0b584edf58fb94e69b
eotkd4791 <eotkd4791@gmail.com>
2022-02-13 18:44:18 +0900

    Update  text

--- coding-sabu-frontend/src/pages/NotFound/index.tsx
+++ coding-sabu-frontend/src/pages/NotFound/index.tsx
@@ -28,8 +28,8 @@ const NotFound: FC = () => {
 
   return (
     <Container>
-      <h2>요청하신 컨텐츠가 없습니다.</h2>
-      <h4>올바른 컨텐츠를 요청해주세요.</h4>
+      <h2>요청하신 컨텐츠 혹은 페이지가 없습니다.</h2>
+      <h4>올바른 자원을 요청해주세요.</h4>
       <img src={codingSabuAlert} alt="Not Found" />
       <ButtonToBack color="yellow" radius={5} height={3} onClick={back}>
         뒤로가기
