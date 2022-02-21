545a2b7d3bd128b3a20df13874393c72a556d398
eotkd4791 <eotkd4791@gmail.com>
2022-02-22 04:54:08 +0900

    Update logout page

--- coding-sabu-frontend/src/pages/Logout/index.tsx
+++ coding-sabu-frontend/src/pages/Logout/index.tsx
@@ -1,10 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import { FC, useState, useCallback } from 'react';
+import { FC, useState, useCallback, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { css } from '@emotion/react';
 import { createActionLogout } from 'store/auth';
-import useTimeout from 'hooks/useTimeout';
 import Loader from 'styles/Loader';
 import { colors } from 'styles/theme';
 
@@ -19,12 +18,14 @@ const Logout: FC = () => {
   const [readyToRedirect, setReadyToRedirect] = useState<boolean>(false);
   const dispatch = useDispatch();
 
-  const logout = useCallback(() => {
-    dispatch(createActionLogout());
+  const logout = useCallback(async () => {
+    await dispatch(createActionLogout());
     setReadyToRedirect(true);
   }, [dispatch]);
 
-  useTimeout(logout, 500);
+  useEffect(() => {
+    logout();
+  }, [logout]);
 
   return (
     <div css={style}>
