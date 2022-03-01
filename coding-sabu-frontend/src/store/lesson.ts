023362919d8a79d58aa3a98b9af24cf2f4e4af0c
eotkd4791 <eotkd4791@gmail.com>
2022-03-01 17:58:47 +0900

    Add actions

--- coding-sabu-frontend/src/store/lesson.ts
+++ coding-sabu-frontend/src/store/lesson.ts
@@ -2,7 +2,13 @@ import { ThunkAsyncState } from '.';
 import { ThunkAction } from 'redux-thunk';
 import { produce } from 'immer';
 import { Lesson } from 'types';
-import { fetchLessonList } from 'apis';
+import {
+  createLesson,
+  fetchLessonList,
+  fetchMyLessonList,
+  fetchOneLesson,
+  joinLesson
+} from 'apis';
 
 // constants
 const FETCH_LESSONS = 'lesson/FETCH_LESSONS' as const;
@@ -11,11 +17,21 @@ const FETCH_LESSONS_FAIL = 'lesson/FETCH_LESSONS_FAIL' as const;
 const FETCH_MY_LESSONS = 'lesson/FETCH_MY_LESSONS' as const;
 const FETCH_MY_LESSONS_SUCCESS = 'lesson/FETCH_MY_LESSONS_SUCCESS' as const;
 const FETCH_MY_LESSONS_FAIL = 'lesson/FETCH_MY_LESSONS_FAIL' as const;
+const CREATE_LESSON = 'lesson/CREATE_LESSON' as const;
+const CREATE_LESSON_SUCCESS = 'lesson/CREATE_LESSON_SUCCESS' as const;
+const CREATE_LESSON_FAIL = 'lesson/CREATE_LESSON_FAIL' as const;
+const FETCH_ONE_LESSON = 'lesson/FETCH_ONE_LESSON' as const;
+const FETCH_ONE_LESSON_SUCCESS = 'lesson/FETCH_ONE_LESSON_SUCCESS' as const;
+const FETCH_ONE_LESSON_FAIL = 'lesson/FETCH_ONE_LESSON_FAIL' as const;
+const JOIN_LESSON = 'lesson/JOIN_LESSON' as const;
+const JOIN_LESSON_SUCCESS = 'lesson/JOIN_LESSON_SUCCESS' as const;
+const JOIN_LESSON_FAIL = 'lesson/JOIN_LESSON_FAIL' as const;
 
 // types
 export interface State {
   readonly lessons: ThunkAsyncState<Lesson[]>;
   readonly mylessons: ThunkAsyncState<Lesson[]>;
+  readonly lessonDetailInfo: ThunkAsyncState<Lesson>;
 }
 
 type Action =
@@ -24,13 +40,22 @@ type Action =
   | { type: typeof FETCH_LESSONS_FAIL; payload: Error }
   | { type: typeof FETCH_MY_LESSONS }
   | { type: typeof FETCH_MY_LESSONS_SUCCESS; payload: Lesson[] }
-  | { type: typeof FETCH_MY_LESSONS_FAIL; payload: Error };
+  | { type: typeof FETCH_MY_LESSONS_FAIL; payload: Error }
+  | { type: typeof CREATE_LESSON }
+  | { type: typeof CREATE_LESSON_SUCCESS }
+  | { type: typeof CREATE_LESSON_FAIL }
+  | { type: typeof FETCH_ONE_LESSON }
+  | { type: typeof FETCH_ONE_LESSON_SUCCESS; payload: Lesson }
+  | { type: typeof FETCH_ONE_LESSON_FAIL; payload: Error }
+  | { type: typeof JOIN_LESSON }
+  | { type: typeof JOIN_LESSON_SUCCESS; payload: Lesson }
+  | { type: typeof JOIN_LESSON_FAIL; payload: Error };
 
-type FetchLessonsThunkAction = ThunkAction<void, State, null, Action>;
+type LessonThunkAction = ThunkAction<void, State, null, Action>;
 
 // action creators
 export const createActionFetchLessons =
-  (): FetchLessonsThunkAction => async dispatch => {
+  (): LessonThunkAction => async dispatch => {
     dispatch({ type: FETCH_LESSONS });
     try {
       const data = await fetchLessonList();
@@ -41,13 +66,49 @@ export const createActionFetchLessons =
   };
 
 export const createActionFetchMyLessons =
-  (): FetchLessonsThunkAction => async dispatch => {
+  (): LessonThunkAction => async dispatch => {
     dispatch({ type: FETCH_MY_LESSONS });
     try {
-      const data = await fetchLessonList();
+      const data = await fetchMyLessonList();
       dispatch({ type: FETCH_MY_LESSONS_SUCCESS, payload: data.lessons });
     } catch (error) {
       dispatch({ type: FETCH_MY_LESSONS_FAIL, payload: error as Error });
+    }
+  };
+
+export const createActionCreateLesson =
+  (lesson: FormData): LessonThunkAction =>
+  async dispatch => {
+    dispatch({ type: CREATE_LESSON });
+    try {
+      await createLesson(lesson);
+      dispatch({ type: CREATE_LESSON_SUCCESS });
+    } catch (error) {
+      dispatch({ type: CREATE_LESSON_FAIL });
+    }
+  };
+
+export const createActionFetchOneLesson =
+  (id: number): LessonThunkAction =>
+  async dispatch => {
+    dispatch({ type: FETCH_ONE_LESSON });
+    try {
+      const lesson = await fetchOneLesson(id);
+      dispatch({ type: FETCH_ONE_LESSON_SUCCESS, payload: lesson });
+    } catch (error) {
+      dispatch({ type: FETCH_ONE_LESSON_FAIL, payload: error as Error });
+    }
+  };
+
+export const createActionJoinLesson =
+  (lessonId: number, userId: number): LessonThunkAction =>
+  async dispatch => {
+    dispatch({ type: JOIN_LESSON });
+    try {
+      const myNewLesson = await joinLesson(lessonId, userId);
+      dispatch({ type: JOIN_LESSON_SUCCESS, payload: myNewLesson.lesson });
+    } catch (error) {
+      dispatch({ type: JOIN_LESSON_FAIL, payload: error as Error });
     }
   };
 
@@ -59,6 +120,11 @@ const initialState: State = {
     error: null
   },
   mylessons: {
+    loading: false,
+    data: null,
+    error: null
+  },
+  lessonDetailInfo: {
     loading: false,
     data: null,
     error: null
