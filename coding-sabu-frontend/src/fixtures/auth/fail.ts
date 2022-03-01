f348be2612a0f97179ee273cc806facf2d32ca54
eotkd4791 <eotkd4791@gmail.com>
2022-03-01 17:53:40 +0900

    Add constants

---
+++ coding-sabu-frontend/src/fixtures/auth/fail.ts
@@ -0,0 +1,7 @@
+const AUTH_FAIL = {
+  LOGIN: '로그인 실패',
+  SIGNUP: '회원가입 실패',
+  REQUIRED_LOGIN: '로그인이 필요합니다.'
+} as const;
+
+export default AUTH_FAIL;
---
+++ coding-sabu-frontend/src/fixtures/lesson/fail.ts
@@ -0,0 +1,6 @@
+const LESSON_FAIL = {
+  OPEN: '수업 개설 실패',
+  REGISTER: '수업 등록 실패'
+} as const;
+
+export default LESSON_FAIL;
