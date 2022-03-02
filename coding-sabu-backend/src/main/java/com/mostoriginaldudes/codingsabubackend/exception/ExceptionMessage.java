52f117f5bf5fd15a3153155e4d6a9d9b37f6c656
eotkd4791 <eotkd4791@gmail.com>
2022-03-03 07:29:54 +0900

    Replace error message to enum

--- coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/exception/ExceptionMessage.java
+++ coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/exception/ExceptionMessage.java
@@ -1,2 +1,12 @@
-package com.mostoriginaldudes.codingsabubackend.exception;public enum ExceptionMessage {
+package com.mostoriginaldudes.codingsabubackend.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ExceptionMessage {
+  FAIL_FILE_UPLOAD("파일 업로드 과정에서 문제가 발생했습니다.");
+
+  private final String exceptionMessage;
 }
