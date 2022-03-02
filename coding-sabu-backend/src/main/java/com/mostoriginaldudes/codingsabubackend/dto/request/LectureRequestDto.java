a377c89b7194e96793c55c1af80be511ba8c1331
eotkd4791 <eotkd4791@gmail.com>
2022-03-03 07:27:51 +0900

    Add dtos

---
+++ coding-sabu-backend/src/main/java/com/mostoriginaldudes/codingsabubackend/dto/request/LectureRequestDto.java
@@ -0,0 +1,11 @@
+package com.mostoriginaldudes.codingsabubackend.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LectureRequestDto {
+  private String unit;
+  private String content;
+}
