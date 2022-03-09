bf892f33d77af6069609707b1b04241a8d1c2e9f
eotkd4791 <eotkd4791@gmail.com>
2022-03-10 04:04:21 +0900

    Add deploy options

--- coding-sabu-frontend/next.config.js
+++ coding-sabu-frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizeFonts: true
+  optimizeFonts: true,
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === 'production'
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production'
+  }
 };
 
 module.exports = nextConfig;
