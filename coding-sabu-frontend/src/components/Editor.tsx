8a7362f24285171713556a818c03350f50ecff15
eotkd4791 <eotkd4791@gmail.com>
2022-04-01 02:11:04 +0900

    Move css module

--- coding-sabu-frontend/src/components/Editor.tsx
+++ coding-sabu-frontend/src/components/Editor.tsx
@@ -4,11 +4,6 @@ import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import Prism from 'prismjs';
 
-import '@toast-ui/editor/dist/toastui-editor.css';
-import 'prismjs/themes/prism.css';
-import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-
 interface Props {
   setValue: (value: string) => void;
 }
--- coding-sabu-frontend/src/pages/_app.tsx
+++ coding-sabu-frontend/src/pages/_app.tsx
@@ -1,3 +1,8 @@
+import '@toast-ui/editor/dist/toastui-editor.css';
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+
 import type { AppProps } from 'next/app';
 import { useStore } from 'react-redux';
 import { persistStore } from 'redux-persist';
