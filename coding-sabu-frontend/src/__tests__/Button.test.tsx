d8d57fcda039868265b347a684bbc258d2fc8d25
eotkd4791 <eotkd4791@gmail.com>
2022-04-05 06:10:39 +0900

    Add configuration for test

--- coding-sabu-frontend/src/__tests__/Button.test.tsx
+++ coding-sabu-frontend/src/__tests__/Button.test.tsx
@@ -1,32 +1,45 @@
 import { render, screen } from '@testing-library/react';
+import { matchers } from '@emotion/jest';
 import Button from 'components/Button';
+import { colors } from 'styles/modules/theme';
+
+expect.extend(matchers);
 
 describe('<Button /> UI 테스트', () => {
-  test('버튼 색깔은 노랑', () => {
-    render(<Button color="yellow">버튼</Button>);
+  describe('color prop이 잘 전달됨', () => {
+    test('버튼 색깔은 노랑', () => {
+      render(<Button color="yellow">버튼</Button>);
 
-    const ButtonComponent = screen.getByRole('button');
-    expect(ButtonComponent).toMatchSnapshot();
+      const ButtonComponent = screen.getByRole('button');
+      expect(ButtonComponent).toHaveStyleRule('background-color', colors.yellow[4]);
+    });
+
+    test('버튼 색깔은 검정', () => {
+      render(<Button color="black">버튼</Button>);
+
+      const ButtonComponent = screen.getByRole('button');
+      expect(ButtonComponent).toHaveStyleRule('background-color', colors.black);
+    });
+
+    test('버튼 색깔은 하양', () => {
+      render(<Button color="white">버튼</Button>);
+
+      const ButtonComponent = screen.getByRole('button');
+      expect(ButtonComponent).toHaveStyleRule('background-color', colors.white);
+    });
   });
 
-  test('버튼 색깔은 검정', () => {
-    render(<Button color="black">버튼</Button>);
+  describe('render prop이 잘 전달됨', () => {
+    test('버튼 텍스트 렌더링', () => {
+      render(<Button color="white">버튼</Button>);
 
-    const ButtonComponent = screen.getByRole('button');
-    expect(ButtonComponent).toMatchSnapshot();
+      const ButtonComponent = screen.getByRole('button');
+      expect(ButtonComponent).toHaveTextContent('버튼');
+    });
   });
 
-  test('버튼 색깔은 하양', () => {
-    render(<Button color="white">버튼</Button>);
-
-    const ButtonComponent = screen.getByRole('button');
-    expect(ButtonComponent).toMatchSnapshot();
-  });
-
-  test('버튼 텍스트 렌더링', () => {
-    render(<Button color="white">버튼</Button>);
-
-    const ButtonComponent = screen.getByRole('button');
-    expect(ButtonComponent).toHaveTextContent('버튼');
-  });
+  // describe('radius prop', () => {});
+  // describe('height prop', () => {});
+  // describe('hover 테스트', () => {});
+  // describe('active 테스트', () => {});
 });
