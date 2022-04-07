80120b1ec52b850b84f641788c7fd29ec890efee
eotkd4791 <eotkd4791@gmail.com>
2022-04-07 23:43:12 +0900

    Add test for <Button /> component

--- coding-sabu-frontend/src/__tests__/Button.test.tsx
+++ coding-sabu-frontend/src/__tests__/Button.test.tsx
@@ -1,45 +1,56 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { matchers } from '@emotion/jest';
 import Button from 'components/Button';
 import { colors } from 'styles/modules/theme';
 
 expect.extend(matchers);
 
-describe('<Button /> UI 테스트', () => {
-  describe('color prop이 잘 전달됨', () => {
-    test('버튼 색깔은 노랑', () => {
-      render(<Button color="yellow">버튼</Button>);
+const onClick = jest.fn();
 
-      const ButtonComponent = screen.getByRole('button');
+const renderButton = (color: 'yellow' | 'white' | 'black') => {
+  render(
+    <Button color={color} onClick={onClick}>
+      버튼
+    </Button>
+  );
+  return screen.getByRole('button');
+};
+
+describe('<Button /> UI 테스트', () => {
+  describe('color prop이 잘 전달된다.', () => {
+    test('버튼 색깔은 노란색이다.', () => {
+      const ButtonComponent = renderButton('yellow');
       expect(ButtonComponent).toHaveStyleRule('background-color', colors.yellow[4]);
     });
 
-    test('버튼 색깔은 검정', () => {
-      render(<Button color="black">버튼</Button>);
-
-      const ButtonComponent = screen.getByRole('button');
+    test('버튼 색깔은 검은색이다.', () => {
+      const ButtonComponent = renderButton('black');
       expect(ButtonComponent).toHaveStyleRule('background-color', colors.black);
     });
 
-    test('버튼 색깔은 하양', () => {
-      render(<Button color="white">버튼</Button>);
-
-      const ButtonComponent = screen.getByRole('button');
+    test('버튼 색깔은 하얀색이다.', () => {
+      const ButtonComponent = renderButton('white');
       expect(ButtonComponent).toHaveStyleRule('background-color', colors.white);
     });
   });
 
-  describe('render prop이 잘 전달됨', () => {
-    test('버튼 텍스트 렌더링', () => {
-      render(<Button color="white">버튼</Button>);
-
-      const ButtonComponent = screen.getByRole('button');
+  describe('props 잘 전달된다.', () => {
+    test('버튼 글자 보인다.', () => {
+      const ButtonComponent = renderButton('white');
       expect(ButtonComponent).toHaveTextContent('버튼');
     });
-  });
 
-  // describe('radius prop', () => {});
-  // describe('height prop', () => {});
-  // describe('hover 테스트', () => {});
-  // describe('active 테스트', () => {});
+    test('버튼이 한번 클릭된다.', () => {
+      const ButtonComponent = renderButton('white');
+      userEvent.click(ButtonComponent);
+      expect(onClick.mock.calls.length).toBe(1);
+    });
+
+    test('onClick으로 전달된 함수가 호출된다.', () => {
+      const ButtonComponent = renderButton('white');
+      userEvent.click(ButtonComponent);
+      expect(onClick).toBeCalled();
+    });
+  });
 });
